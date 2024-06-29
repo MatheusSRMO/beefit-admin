2d274b23af83f02b969eee5a585c94fd02526956
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-29 11:50:44 -0300

    refactor: Update import paths for SideBar and NavBar components
    The code changes in this commit update the import paths for the SideBar and NavBar components. The SideBar component is now imported from "@/components/custom/side-bar" instead of "@/components/SideBar". Similarly, the NavBar component is imported from "@/components/custom/nav-bar" instead of "@/components/NavBar". This change ensures that the correct components are imported and used in the layout file.
    
    Based on the recent repository commits, the commit message follows the convention of starting with a verb indicating the type of change (e.g., feat for feature, fix for bug fix).

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import SideBar from "@/components/SideBar";
+import SideBar from "@/components/custom/side-bar";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import NavBar from "@/components/custom/nav-bar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,13 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={cn(inter.className, "flex flex-row h-screen bg-background")}>
-        <SideBar className="h-full w-72"/>
+        <SideBar className="h-full w-72 z-10" />
         <ScrollArea className="h-full flex-1">
-          {children}
+          <NavBar />
+          <div className="flex-1 mt-32">
+
+            {children}
+          </div>
         </ScrollArea>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image";
+import NavBar from "@/components/custom/nav-bar";
 
 export default function Home() {
   return (
-    <main className="h-screen p-20">
-      <div className="flex flex-row justify-center items-center w-1/3 gap-4">
-        <Avatar className="h-20 w-20 border-[#528AA5] border-4">
-          <AvatarImage src="https://github.com/jjakob10.png" alt="Imagem" />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar>
-        <div className="flex-1">
-          <h1 className="flex-1 text-4xl font-bold text-[#FFDC98] w-32">Johann</h1>
-          <h1 className="flex-1 text-3xl font-bold text-[#FFDC98] w-64">Jakob</h1>
-        </div>
-      </div>
+    <main className="h-screen px-10 pb-10">
+      
+      {
+        Array(50).fill(0).map((_, index) => (
+          <div key={index} className="h-20 bg-slate-500 rounded-lg my-5" />
+        ))
+      }
     </main>
   );
 }
