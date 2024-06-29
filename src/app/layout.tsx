2bd5ced86111f3fd9cae1f6b655608ed6713a4b3
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-29 20:14:36 -0300

    refactor: Remove unnecessary components from layout
    The code changes in this commit remove the SideBar, ScrollArea, and NavBar components from the layout. These components are no longer needed as the children components are now rendered directly in the body element. This simplifies the code and improves performance by reducing the number of unnecessary components.
    
    Based on the recent repository commits, the commit message follows the convention of starting with a verb indicating the type of change (e.g., feat for feature, fix for bug fix).

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import SideBar from "@/components/custom/side-bar";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import NavBar from "@/components/custom/nav-bar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,14 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={cn(inter.className, "flex flex-row h-screen bg-background")}>
-        <SideBar className="h-full w-72 z-10" />
-        <ScrollArea className="h-full flex-1">
-          <NavBar />
-          <div className="flex-1 mt-32">
-
-            {children}
-          </div>
-        </ScrollArea>
+        {children}
       </body>
     </html>
   );
