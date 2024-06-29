c557d85da1763cff5836d0aaa0a457756c2d8905
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-29 20:13:48 -0300

    feat: Add ExercisePage component for displaying exercises
    The code changes in this commit add a new component called ExercisePage. This component is responsible for rendering a page that displays a list of exercises. It imports the ExerciseCard component to render each exercise's details. The exercises are stored in an array and mapped over to create ExerciseCard instances. The component also includes a TitleSection component with a button to add a new exercise. The router is used to navigate to the add exercise page when the button is clicked.
    
    Based on the recent repository commits, the commit message follows the convention of starting with a verb indicating the type of change (e.g., feat for feature, fix for bug fix).

---
+++ src/app/(auth)/exercicios/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import React from 'react'
+import TitleSection from '@/components/custom/title-section';
+import { useRouter } from 'next/navigation';
+import { ExerciseCard } from '@/components/custom/exercise-card';
+
+const exercises = [
+  {
+    id: 1,
+    name: 'Bicep Curl (Dumbbell)',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/02941201-Dumbbell-Biceps-Curl_Upper-Arms.mp4',
+    observation: 'Observação do exercício Observação do exercício Observação do exercício Observação do exercícioObservação do exercício Observação do exercício Observação do exercício Observação do exercícioObservação do exercício Observação do exercício Observação do exercício Observação do exercícioObservação do exercício Observação do exercício Observação do exercício Observação do exercícioObservação do exercício Observação do exercício Observação do exercício Observação do exercícioObservação do exercício Observação do exercício Observação do exercício Observação do exercício'
+  },
+  {
+    id: 2,
+    name: 'Dumbbell Bench Press Chest',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/02891201-Dumbbell-Bench-Press_Chest.mp4',
+    observation: 'Observação'
+  },
+  {
+    id: 3,
+    name: 'Barbell Bench Press Chest',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/00251201-Barbell-Bench-Press_Chest.mp4',
+    observation: 'Observação'
+  },
+  {
+    id: 4,
+    name: 'Dumbbell Flyes Chest',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/07391201-Sled-45-Leg-Press_Hips.mp4',
+    observation: 'Observação'
+  },
+  {
+    id: 5,
+    name: 'Dumbbell Shoulder Press Shoulders',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/05991201-Lever-Seated-Leg-Curl_Thighs.mp4',
+    observation: 'Observação'
+  },
+  {
+    id: 6,
+    name: 'Dumbbell Lateral Raise Shoulders',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/03121201-Dumbbell-Hammer-Curl-(version-2)_Upper-Arms.mp4',
+    observation: 'Observação'
+  },
+  {
+    id: 7,
+    name: 'Dumbbell Front Raise Shoulders',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/00321201-Barbell-Deadlift_Hips-FIX.mp4',
+    observation: 'Observação'
+  },
+  {
+    id: 8,
+    name: 'Dumbbell Shrugs Shoulders',
+    series: 3,
+    repetitions: 10,
+    weight: 50,
+    url: 'https://pump-app.s3.eu-west-2.amazonaws.com/exercise-assets/12691201-Cable-Standing-Up-Straight-Crossovers_Chest.mp4',
+    observation: 'Observação'
+  },
+]
+
+
+export default function ExercisePage() {
+  const router = useRouter();
+  return (
+    <main className="min-h-[calc(100vh_-_245px)] px-10 pb-10">
+      
+      <TitleSection title="Exercícios">
+        <TitleSection.Button title="Acidionar Exercício" onClick={() => {
+          router.push('/exercicios/adicionar');
+        }} />
+      </TitleSection>
+
+      <div className="flex flex-wrap gap-10 py-10 justify-center">
+        {
+          exercises.map(exercise => (
+            <ExerciseCard key={exercise.id} {...exercise} />
+          ))
+        }
+      </div>
+    </main>
+  )
+}
