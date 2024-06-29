7975d0eefd6a034e1812f643b33f9df58aa71787
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-29 20:13:17 -0300

    refactor: Update NavBar component layout and styling
    The code changes in this commit refactor the layout and styling of the NavBar component. The component is updated to use flexbox for better alignment and spacing of its child elements. The avatar and profile information are now displayed in a single column, making better use of the available space. Additionally, the button icons are adjusted to have a consistent size and color.
    
    Based on the recent repository commits, the commit message follows the convention of starting with a verb indicating the type of change (e.g., feat for feature, fix for bug fix).

--- src/components/custom/nav-bar.tsx
+++ src/components/custom/nav-bar.tsx
@@ -1,32 +1,23 @@
 import React from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from '../ui/input';
 import { BellDotIcon, SettingsIcon } from 'lucide-react';
 import { Button } from '../ui/button';
+import Profile from './profile';
 
 export default function NavBar() {
   return (
-    <div className="fixed right-0 left-60 py-2 pl-20 pr-10 flex  bg-[#1e1558] justify-between">
-      <div className="flex justify-center items-center w-1/3 gap-4">
-        <Avatar className="h-20 w-20 border-[#528AA5] border-4">
-          <AvatarImage src="https://github.com/MatheusSRMO.png" alt="Imagem" />
-          <AvatarFallback>A</AvatarFallback>
-        </Avatar>
-        <div className="flex-1">
-          <h1 className="flex-1 text-3xl font-bold text-primary w-32">Matheus</h1>
-          <h1 className="flex-1 text-2xl font-bold text-primary w-64">Ribeiro</h1>
-        </div>
-      </div>
+    <div className="fixed right-2 left-60 py-2 pl-20 pr-7 flex z-40 bg-[#1e1558] justify-between rounded-br-3xl">
+      <Profile url="https://github.com/MatheusSRMO.png" firstName='Matheus' lastName='Ribeiro' />
 
       <div className="flex items-center gap-3">
         <Input placeholder="O que procura?" className='border-none text-primary placeholder:text-primary bg-background w-96' />
 
         <Button variant="link" size="icon">
-          <BellDotIcon className="w-8 h-8 text-primary" />
+          <BellDotIcon className="text-primary" />
         </Button>
 
         <Button variant="link" size="icon">
-          <SettingsIcon className="w-8 h-8 text-primary" />
+          <SettingsIcon className="text-primary" />
         </Button>
 
       </div>
