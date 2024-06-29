ffd61f68b03dc76d4d06e617943564d2e78cc7d4
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-29 20:14:23 -0300

    feat: Add layout component for authentication pages
    The code changes in this commit add a new layout component for authentication pages. The layout component includes a sidebar, a scrollable area, and a navigation bar. It also handles the rendering of the children components passed to it. This layout component improves the structure and organization of the authentication pages, providing a consistent layout across different pages.
    
    Based on the recent repository commits, the commit message follows the convention of starting with a verb indicating the type of change (e.g., feat for feature, fix for bug fix).

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from 'react'
+import SideBar from "@/components/custom/side-bar";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import NavBar from "@/components/custom/nav-bar";
+import { Button } from "@/components/ui/button";
+import { ArrowBigLeft } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const router = useRouter();
+  return (
+    <>
+      <SideBar className="h-full w-72 z-50" labels={[
+        {
+          href: "/alunos",
+          label: "Alunos",
+        },
+        {
+          href: "/exercicios",
+          label: "Exercícios",
+        },
+      ]} />
+      <ScrollArea className="h-full flex-1">
+        <NavBar />
+        <div className="flex-1 mt-28">
+          {
+            pathname.split("/").length > 2 && (
+
+              <Button variant='link' onClick={() => router.back()} className='absolute mt-5'>
+                <ArrowBigLeft className='w-4 h-4 text-primary' />
+              </Button>
+            )
+          }
+          {children}
+        </div>
+      </ScrollArea>
+    </>
+  )
+}
