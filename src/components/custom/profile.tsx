f40918f467d51908365f26c07644e17b0cda75ee
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-30 18:56:48 -0300

    feat: Add AuthLayout component for authentication pages

--- src/components/custom/profile.tsx
+++ src/components/custom/profile.tsx
@@ -1,24 +1,51 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import clsx from 'clsx';
+import { Aluno } from '@prisma/client';
+import Image from 'next/image';
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 export interface ProfileProps {
   size?: 'md' | 'lg'
-  url: string
+  url?: string | null
   firstName: string
-  lastName: string   
+  lastName: string
 }
 
 export default function Profile({ url, firstName, lastName, size = 'lg' }: ProfileProps) {
+  console.log(url);
   return (
     <div className="flex justify-center items-center max-w-1/3 min-w-44 gap-4">
-      <Avatar className={clsx("border-[#528AA5] border-4", {
-        'h-16 w-16': size === 'md',
-        'h-20 w-20': size === 'lg',
-      })}>
-        <AvatarImage src={url} alt="Imagem" />
-        <AvatarFallback>Profile</AvatarFallback>
-      </Avatar>
+
+      {
+        size === 'md' ? (
+          <Image
+            src={url || ''}
+            alt="Imagem"
+            width={100}
+            height={100}
+            className="border-[#528AA5] border-4 rounded-full h-16 w-16"
+          />
+        ) : (
+          <div className='w-20 h-20'>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton appearance={{
+                elements: {
+                  avatarBox: {
+                    width: '100%',
+                    height: '100%',
+                    border: '4px solid #528AA5',
+                  }
+                }
+              }} />
+            </SignedIn>
+          </div>
+        )
+      }
+
       <div className="flex-1">
         <h1 className={clsx("flex-1 font-bold text-primary max-w-32", {
           'text-2xl': size === 'md',
@@ -29,6 +56,6 @@ export default function Profile({ url, firstName, lastName, size = 'lg' }: Profi
           'text-2xl': size === 'lg',
         })}>{lastName}</h2>
       </div>
-    </div>
+    </div >
   )
 }
