71dc380476192229c5791a0493bfdbcba9b25f46
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-30 16:52:40 -0300

    refactor: Update headers function in next.config.mjs to enable CORS for API routes

--- next.config.mjs
+++ next.config.mjs
@@ -15,19 +15,26 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
-                // https://vercel.com/guides/how-to-enable-cors
-                source: "/api/:path*",
+                // Routes this applies to
+                source: "/api/(.*)",
+                // Headers
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    // Allow for specific domains to have access or * for all
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                        // DOES NOT WORK
+                        // value: process.env.ALLOWED_ORIGIN,
+                    },
+                    // Allows for specific methods accepted
                     {
                         key: "Access-Control-Allow-Methods",
-                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
                     },
+                    // Allows for specific headers accepted (These are a few standard ones)
                     {
                         key: "Access-Control-Allow-Headers",
-                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                        value: "Content-Type, Authorization",
                     },
                 ],
             },
