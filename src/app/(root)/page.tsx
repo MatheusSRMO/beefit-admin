61112367ad85f45ee708f382f18e1ca41c343d06
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-07-01 00:58:34 -0300

    refactor: Update personalTrainerId in TrainerRegisterProps
    The code changes in this commit add the personalTrainerId property to the TrainerRegisterProps type in the types.ts file. This property is required and represents the ID of the personal trainer associated with the trainer registration. This change ensures that the personalTrainerId is properly defined and passed when registering a new trainer.
    
    Based on the recent repository commits, the commit message follows the convention of starting with a verb indicating the type of change (e.g., feat for feature, fix for bug fix).

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,22 +1,13 @@
-import Profile, { ProfileProps } from "@/components/custom/profile";
-import TitleSection from "@/components/custom/title-section";
-import { Button } from "@/components/ui/button";
-import { BellRingIcon, CalendarIcon } from "lucide-react";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default function Home() {
+
+  redirect("/alunos");
+
   return (
     <main className="h-screen px-10 pb-10">
 
-      <TitleSection title="Treinos">
-        <TitleSection.Button title="Acidionar Treino" />
-      </TitleSection>
-
-      {
-        Array(50).fill(0).map((_, index) => (
-          <div key={index} className="h-20 bg-slate-500 rounded-lg my-5" />
-        ))
-      }
     </main>
   );
 }
